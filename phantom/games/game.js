803013310e3efb158d32a68e277e0fd38e14d430
Christopher-Wadsworth <80176215+Christopher-Wadsworth@users.noreply.github.com>
2026-02-12 20:05:50 -0600

    Update game.js

--- phantom/games/game.js
+++ phantom/games/game.js
@@ -27,7 +27,7 @@ let wallDeathImg = new Image()
                 wallDeathImg.src = "WallDeath.png";
 
 let boardImg = new Image()
-                boardImg.src = "SnakeBackgroundSolid.png";
+                boardImg.src = "SnakeBackgroundGrace.png";
 
 window.onload = function () {
     // Set board height and width
