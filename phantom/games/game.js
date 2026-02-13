7f56204d816cab53341c9f67f0ebfde3667b321e
Christopher-Wadsworth <80176215+Christopher-Wadsworth@users.noreply.github.com>
2026-02-12 18:56:25 -0600

    Update game.js

--- phantom/games/game.js
+++ phantom/games/game.js
@@ -1,11 +1,11 @@
 let blockSize = 25;
-let total_row = 17; //total row number
-let total_col = 17; //total column number
+let total_row = 19; //total row number
+let total_col = 19; //total column number
 let board;
 let context;
 
 let snakeX = blockSize * 5;
-let snakeY = blockSize * 5;
+let snakeY = blockSize * 10;
 
 // Set the total number of rows and columns
 let speedX = 0;  //speed of snake in x coordinate.
@@ -137,7 +137,7 @@ function placeFood() {
  function reset(e) {
     if (e.code == "Space" ) {
         snakeX = blockSize * 5
-        snakeY = blockSize * 5
+        snakeY = blockSize * 10
         speedX = 0
         speedY = 0
         Score = 0
