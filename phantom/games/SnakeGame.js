df3a345631362e0ffa98a992905489eb545db363
Christopher-Wadsworth <80176215+Christopher-Wadsworth@users.noreply.github.com>
2026-02-13 22:07:12 -0600

    Update SnakeGame.js

--- phantom/games/SnakeGame.js
+++ phantom/games/SnakeGame.js
@@ -193,7 +193,7 @@ function placeFood() {
 
   function infpoints(e){
     if (e.key == "p"){
-      Score =+ 100;
+      Score += 100;
       document.getElementById("Score").innerText = "Score: " + Score;
     }
   }
