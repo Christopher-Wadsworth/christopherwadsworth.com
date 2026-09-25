f7eb96025a72a1bedc617fbb9ebcd5135db2e320
Christopher-Wadsworth <80176215+Christopher-Wadsworth@users.noreply.github.com>
2026-09-25 00:25:57 -0500

    Change event listener from heart to envelope

--- phantom/WFam/passwords.js
+++ phantom/WFam/passwords.js
@@ -1,7 +1,7 @@
 const envelope = document.querySelector(".envelope-wrapper");
 const heart = document.querySelector(".heart");
 
-heart.addEventListener("click", () => {
+envelope.addEventListener("click", () => {
     envelope.classList.toggle("flap");
 });
 
